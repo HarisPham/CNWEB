eed8c4296e667de391431c45c8e911effe80442a
HarisPham <hakuriuhafini@gmail.com>
2024-11-25 14:29:47 +0700

    Add files via upload

---
+++ BTTH01/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quản lý sản phẩm</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+<header>
+    <nav>
+        <ul>
+            <li><a href="index.php?role=admin" id="admin"><b>Administration</b></a></li>
+            <li><a href="index.php?role=user" id="user"><b>User</b></a></li>
+            <li><a href="index.php"><b>Trang chủ</b></a></li>
+        </ul>
+    </nav>
+</header>
